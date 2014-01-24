4661388a62aad040c37027b4718b86c7fded077d
ImDaeLim <blackway76@gmail.com>
2014-01-24 11:10:27 +0900

    jquery.min.map 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,25 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Board(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+
+class Comment(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board)
+
+"""
+Board 테이블
+제목
+내용
+생성일자
+수정일자
+
+"""
